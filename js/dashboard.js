f6fc358b18d69ccbdf71d5d773e52f3e14ed1f54
mhille813 <320678190+mhille813@users.noreply.github.com>
2026-09-17 16:47:05 +0200

    Make pie chart functional

--- js/dashboard.js
+++ js/dashboard.js
@@ -17,11 +17,13 @@ window.onload = function () {
         if (!isNaN(gradeText)) {
             if (gradeText >= 5.5) {
                 gradePassed = true;
-                gradeElement.classList.add("passed");
+                gradeElement.classList.add("gradePassed");
             } else {
                 gradeFailed = true;
-                gradeElement.classList.add("failed");
+                gradeElement.classList.add("gradeFailed");
             }
+        } else {
+            gradeElement.classList.add("gradeAvailable");
         }
 
         const studyPointsElement = tableRow.getElementsByClassName("studyPoints")[0];
@@ -37,16 +39,41 @@ window.onload = function () {
         }
     }
 
-    // TODO:
-    // - study points progress
-    // - study advice boundary
     const availableStudyPoints = totalStudyPoints - ownedStudyPoints - failedStudyPoints;
-    console.log(ownedStudyPoints);
-    console.log(failedStudyPoints);
-    console.log(totalStudyPoints);
-    console.log(availableStudyPoints);
-    // https://www.geeksforgeeks.org/web-templates/how-to-create-a-pie-chart-using-html-css/
 
-    const pieChart = document.getElementById("pieChart");
-    pieChart.style = "background-color: #a0a0a0;"
+    if (totalStudyPoints == 0) {
+        pieChart.style = "display: none;"
+    } else {
+        const pieChart = document.getElementsByClassName("pieChart")[0];
+        let pieChartStyle = "background-image: conic-gradient("
+        let currentAngle = 0;
+        if (ownedStudyPoints) {
+            const angle = ownedStudyPoints / totalStudyPoints * 360 + currentAngle;
+            const color = "var(--grade-passed-color)";
+            pieChartStyle += `${color} ${currentAngle}deg, ${color} ${angle}deg, `;
+            currentAngle = angle;
+        }
+        if (failedStudyPoints) {
+            const angle = failedStudyPoints / totalStudyPoints * 360 + currentAngle;
+            const color = "var(--grade-failed-color)";
+            pieChartStyle += `${color} ${currentAngle}deg, ${color} ${angle}deg, `;
+            currentAngle = angle;
+        }
+        if (availableStudyPoints) {
+            const angle = availableStudyPoints / totalStudyPoints * 360 + currentAngle;
+            const color = "var(--grade-available-color)";
+            pieChartStyle += `${color} ${currentAngle}deg, ${color} ${angle}deg, `;
+            currentAngle = angle;
+        }
+        pieChartStyle = pieChartStyle.slice(0, -2) + ");";
+        pieChart.style = pieChartStyle;
+    }
+
+    // TODO:
+    // - study advice boundary
+    // console.log(ownedStudyPoints);
+    // console.log(failedStudyPoints);
+    // console.log(totalStudyPoints);
+    // console.log(availableStudyPoints);
+    // https://www.geeksforgeeks.org/web-templates/how-to-create-a-pie-chart-using-html-css/
 };
